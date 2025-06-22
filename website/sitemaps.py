992b352adebbc0bd655eba884a0eae71af001092
Abhinandh R Nair <abhinandhrnair333@gmail.com>
2025-06-22 20:25:36 +0530

    Your commit message here

--- website/sitemaps.py
+++ website/sitemaps.py
@@ -1,7 +1,10 @@
 from django.contrib.sitemaps import Sitemap
 from django.shortcuts import reverse
 
+
 class StaticViewSitemap(Sitemap):
+    protocol = 'https'
+
     def items(self):
          return [
             'home',
